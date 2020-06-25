0467925b251cc6ccee3afcc51fd7bb20d3c5ba8a
jagveer-gandhi <jagveer.gandhi@gmail.com>
2020-06-26 01:01:08 +0530

    Added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t jagveer/multi-client:latest -t jagveer/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jagveer/multi-server:latest -t jagveer/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jagveer/multi-worker:latest -t jagveer/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push jagveer/multi-client:latest
+docker push jagveer/multi-server:latest
+docker push jagveer/multi-worker:latest
+
+docker push jagveer/multi-client:$SHA
+docker push jagveer/multi-server:$SHA
+docker push jagveer/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployment/server-deployment server=jagveer/multi-server:$SHA
+kubectl set image deployment/client-deployment server=jagveer/multi-client:$SHA
+kubectl set image deployment/worker-deployment server=jagveer/multi-worker:$SHA
